6394fdcfb98e8f540e049a91411aa4a7d5502232
krnxptr <167197148+krnxptr@users.noreply.github.com>
2025-03-11 11:58:57 +0200

    Update PEVOMAG.php

--- PEVOMAG.php
+++ PEVOMAG.php
@@ -1,2 +1,29 @@
 <?php
+
+function readCSV($filename) {
+    $products = [];
+    if (($handle = fopen($filename, "r")) !== FALSE) {
+        fgetcsv($handle); // Skip the header row
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $products[] = [
+                'name' => $data[0],
+                'description' => $data[1],
+                'price' => $data[2]
+            ];
+        }
+        fclose($handle);
+    }
+    return $products;
+}
+
+$files = ["/Tablets.csv", "/Phones.csv",
+ "/Laptops.csv", "/Chargers.csv"];
+$allProducts = [];
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $allProducts = array_merge($allProducts, readCSV($file));
+    }
+}
+
 ?>
