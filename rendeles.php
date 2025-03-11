0bdab4e206726adb20132a43d33244bc45ea2852
krnxptr <167197148+krnxptr@users.noreply.github.com>
2025-03-11 11:59:23 +0200

    Add files via upload

---
+++ rendeles.php
@@ -0,0 +1,29 @@
+<?php
+$hiba = mysqli_connect("localhost", "root", "");
+mysqli_select_db($hiba, "pevomag");
+
+if (isset($_POST['g1'])) {
+    $f_nev = $_POST['f-name'];
+    $k_nev = $_POST['l-name'];  // Corrected field name
+    $address = $_POST['address'];  // Corrected field name
+    $city = $_POST['city'];
+    $state = $_POST['state'];
+    $zip = $_POST['zip'];
+    $credit_card_no = $_POST['card-num'];
+    $exp = $_POST['expire'];
+    $CVV = $_POST['security'];
+
+    $insert = "INSERT INTO shipping_details (First, Last, Street, City, State, Zip, `Credit card no.`, Exp, CVV) 
+               VALUES ('$f_nev', '$k_nev', '$address', '$city', '$state', '$zip', '$credit_card_no', '$exp', '$CVV')";
+
+    if (mysqli_query($hiba, $insert)) {
+        echo "A rendelést rögzítettük!";
+		echo '<script src="orders.js"></script>';	
+		echo '<input class="vissza" type="button" onclick="submitclick()" value="vissza">';
+
+
+    } else {
+        echo "Hiba történt a rendelés rögzítése során.";
+    }
+}
+?>
